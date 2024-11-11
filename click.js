ed2eea1634636a67c96b89647ce280f15b6a485f
lsg33 <forcesspecial801@gmail.com>
2024-11-11 11:54:19 -0600

    Create click.js

---
+++ click.js
@@ -0,0 +1,56 @@
+document.getElementById('crazyGames').onclick = function() {
+            document.getElementById('gameSelectModal').style.display = 'none';
+            fetchData('https://crazygames.com');
+        };
+
+        document.getElementById('freezenova').onclick = function() {
+            document.getElementById('gameSelectModal').style.display = 'none';
+            fetchData('https://unblocked-games.s3.amazonaws.com/index.html');
+        };
+
+        document.getElementById('coolmethgains').onclick = function() {
+            document.getElementById('gameSelectModal').style.display = 'none';
+            fetchData('https://coolmathgames.com');
+        };
+
+        document.getElementById('gfcenow').onclick = function() {
+            document.getElementById('gameSelectModal').style.display = 'none';
+            fetchData('https://play.geforcenow.com/mall');
+        };
+
+        document.getElementById('robloxGame').onclick = function() {
+            document.getElementById('gameSelectModal').style.display = 'none';
+            fetchData('https://roblox.com');
+        };
+
+        /// social media
+
+        document.getElementById('snapChat').onclick = function() {
+            document.getElementById('gameSelectModal').style.display = 'none';
+            fetchData('https://snapchat.com');
+        };
+
+        document.getElementById('Instagram').onclick = function() {
+            document.getElementById('gameSelectModal').style.display = 'none';
+            fetchData('https://instagram.com');
+        };
+
+        document.getElementById('tiktok').onclick = function() {
+            document.getElementById('gameSelectModal').style.display = 'none';
+            fetchData('https://tiktok.com');
+        };
+
+        document.getElementById('Reddit').onclick = function() {
+            document.getElementById('gameSelectModal').style.display = 'none';
+            fetchData('https://reddit.com');
+        };
+
+        document.getElementById('discord').onclick = function() {
+            document.getElementById('gameSelectModal').style.display = 'none';
+            fetchData('https://discord.com');
+        };
+
+        document.getElementById('youtube').onclick = function() {
+            document.getElementById('gameSelectModal').style.display = 'none';
+            fetchData('https://youtube.com');
+        };
